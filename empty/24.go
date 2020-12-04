b940ec037f49135143952c12e7ab2f3eb72e30c0
刘东旭 <1278083770@qq.com>
2020-12-04 16:05:15 +0800

    整理第一批练习，50.go前

---
+++ empty/24.go
@@ -0,0 +1,9 @@
+// 1.声明两个结构体 Class Student
+
+// 2.实例化 Class 结构体
+// 2.1 将下列切片 nameArr 内容通过循环遍历插入到 Class.Students 字段中
+// nameArr := []string{"张三", "李四", "王五", "赵六"}
+// 2.2 Studnet 的 gender 字段，通过循环 i 的取余决定
+// sex := []string{"男", "女"}
+
+// 3.将这个实例化的结构体转为 json
